7e059f8f1f8d30a06aca0bc301b2652bbf9896e0
Wolf Bruckner <wolf.bruckner@networkedinsights.com>
2018-09-20 17:49:25 -0500

    third

--- Piece.py
+++ Piece.py
@@ -5,7 +5,10 @@ class Piece(object):
         self._y = 0
         self.hp = 0
         self.defense = 0
+        self.mark = ''
 
+    def set_mark(self, mark):
+        self.mark = mark
 
     def set_location(self, x, y):
         self._x = x
@@ -17,3 +20,18 @@ class Piece(object):
     def set_def(self, defense):
         self.defense = defense
 
+    def get_location(self):
+        return (self._x, self._y)
+
+    def get_location_mark(self):
+        return (self._x, self._y, self.mark)
+
+if __name__ == "__main__":
+    from Board import Board
+    tank = Piece()
+    tank.set_location(3, 3)
+    tank.set_mark("x")
+
+    new = Board(5, 5)
+    new.insert(*tank.get_location_mark())
+    new.pnt()
